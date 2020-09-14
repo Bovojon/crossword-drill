59b8ed07cd56113875a4910199223daab235e605
Bovojon <bovojon@gmail.com>
2020-09-13 17:49:09 -0700

    Add initial drill and answer views and html templates

--- xword_data/views.py
+++ xword_data/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
-# Create your views here.
+from xword_data.models import (Entry, Puzzle, Clue)
+
+def drill(request):
+    return render(request, 'tdd_exercise/drill.html')
+
+def answer(request, clue_id):
+    return render(request, 'tdd_exercise/answer.html')
